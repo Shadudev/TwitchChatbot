0e1c3c148dac3f0eee28ae17ffa0a21e80d4e08e
Ofir Cohen <ofirgvc@gmail.com>
2020-03-22 07:59:52 +0200

    Updated dad jokes script, removing unnecessary features

--- scripts/dad_jokes_teller/dad_jokes_teller.py
+++ scripts/dad_jokes_teller/dad_jokes_teller.py
@@ -52,31 +52,7 @@ class DadJokesTeller(CommandHandler):
 		return getattr(chat_message, 'custom_reward_id', None) == COMMAND_REWARD_ID
 
 	def handle_message(self, chat_message):
-		if not chat_message.is_mod:
-			self._handle_basic_command(chat_message)
-		else:
-			operation = chat_message.split(' ')[1].lower()
-			if operation not in PROPERTIES and operation not in [RESET_COOLDOWN_COMMAND, ALL_PROPERTIES_MAGIC]:
-				self._handle_basic_command(chat_message)
-			elif operation == RESET_COOLDOWN_COMMAND:
-				self._handle_cooldown_reset_command()
-			elif 2 == params_count:
-				self._handle_get_command(operation)
-			else:
-				self._handle_set_command(chat_message)
-
-	def _handle_basic_command(self, chat_message):
-		if self._is_command_in_cooldown():
-			raise 'is_command_in_cooldown'
-			#send_message('Command is in cooldown for {} more seconds.'.format(get_remaining_cooldown()))
-		elif self._is_user_in_cooldown(chat_message.user):
-			raise 'is_user_in_cooldown'
-			#send_message('NO {} ! No more for at least {} more seconds!'.format(chat_message.display_name, get_user_remaining_cooldown(chat_message.user)))
-		else:
-			self._tell_joke()
-			#add_command_cooldown()
-			#add_user_cooldown(chat_message.user)
-			self._set_manual_cooldown_reset(False)
+		self._tell_joke()
 
 
 	def _tell_joke(self):
@@ -95,127 +71,8 @@ class DadJokesTeller(CommandHandler):
 		jokes = eval(self._read_file(file_path))
 		index = random.randrange(len(jokes))
 		joke = jokes.pop(index)
-		#self.write_file(file_path, str(jokes))
+		self._write_file(file_path, str(jokes))
 		return joke
-
-
-	def _handle_cooldown_reset_command(self):
-		self._set_manual_cooldown_reset(True)
-		self._send_message("Cooldown was reset!")
-
-
-	def _set_manual_cooldown_reset(self, state):
-		self._set_property(MANUAL_COOLDOWN_RESET_KEY, state)
-
-
-	def _get_manual_cooldown_reset(self):
-		return bool(self._get_property(MANUAL_COOLDOWN_RESET_KEY))
-
-
-	def _is_command_in_cooldown(self):
-		return False
-		#return Parent.IsOnCooldown(COMMAND_ID, '') and not get_manual_cooldown_reset()
-
-	"""
-	def get_remaining_cooldown():
-		return Parent.GetCooldownDuration(COMMAND_ID, '')
-
-	"""
-	def _is_user_in_cooldown(self, user):
-		return False
-		#return Parent.IsOnUserCooldown(COMMAND_ID, '', user) and not get_manual_cooldown_reset()
-
-	"""
-	def get_user_remaining_cooldown(user):
-		return Parent.GetUserCooldownDuration(COMMAND_ID, '', user)
-
-
-	def add_command_cooldown():
-		cooldown = get_command_cooldown()
-		Parent.AddCooldown(COMMAND_ID, '', cooldown)
-
-
-	def add_user_cooldown(user):
-		user_cooldown = get_user_cooldown()
-		Parent.AddUserCooldown(COMMAND_ID, '', user, user_cooldown)
-	"""
-
-	def _handle_get_command(self, config_property):
-		if config_property == ALL_PROPERTIES_MAGIC:
-			config = self._get_properties()
-			for property in config.keys():
-				if property not in PROPERTIES:
-					config.pop(property)
-			self.send_message(str(config))
-		elif config_property in PROPERTIES:
-			self.send_message('{}={}'.format(config_property, self._get_property(config_property)))
-
-
-	def _handle_set_command(self, chat_message):
-		config_property = chat_message.split(' ')[1].lower()
-		new_value = chat_message.split(' ')[2].lower()
-		
-		if config_property not in PROPERTIES:
-			self._show_properties()
-		else:
-			current_value = self._get_property(config_property)
-			try:
-				new_value = type(current_value)(new_value)
-			except:
-				self.send_message('Invalid value {}, expected value of type {}'.format(new_value, type(current_value)))
-			if current_value != new_value:
-				self._set_property(config_property, new_value)
-				self.send_message('{} set to {}'.format(config_property, new_value))
-			else:
-				self.send_message('{} is already {} !'.format(config_property, new_value))
-
-
-	def _get_command_cooldown(self):
-		return self._get_property(COMMAND_COOLDOWN_KEY)
-
-
-	def _get_user_cooldown(self):
-		return self._get_property(USER_COOLDOWN_KEY)
-
-
-	def _get_property(self, config_property):
-		return self._get_properties()[config_property]
-
-
-	def _get_properties(self):
-		return self._read_settings()
-
-
-	def _set_property(self, config_property, new_value):
-		settings = self._read_settings()
-		settings[config_property] = new_value
-		self._update_settings(settings)
-
-
-	def _show_properties(self):
-		self.send_message("Invalid property. Available properties: {}. Use {} for all properties.".format(make_pretty(PROPERTIES), ALL_PROPERTIES_MAGIC))
-
-
-	def _make_pretty(self, collection, separator=', '):
-		return separator.join(collection)
-
-
-	def _read_settings(self):
-		return self._read_json_from_file(SETTINGS_FILE_PATH)
-
-
-	def _update_settings(self, settings):
-		self._dump_json_to_file(SETTINGS_FILE_PATH, settings)
-
-
-	def _dump_json_to_file(self, file_path, file_data):
-		with open(file_path, 'w', encoding='utf8') as f:
-			json.dump(file_data, f, ensure_ascii=False)
-
-
-	def _read_json_from_file(self, file_path):
-		with open(file_path) as f:
-			return json.load(f)
 
 
 	def _read_file(self, path):

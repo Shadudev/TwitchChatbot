f7ba45e87fafe97600c12fe358d042524f6d5e7d
Ofir Cohen <ofirgvc@gmail.com>
2020-03-22 08:00:25 +0200

    Added hats script

--- scripts/scripts.py
+++ scripts/scripts.py
@@ -4,6 +4,7 @@ from scripts.promotional_timer import promotional_timer
 from scripts.dad_jokes_teller import dad_jokes_teller
 from scripts.uwu import uwu, dynamic_dictionary
 from scripts.soundbot import soundbot
+from scripts.hats_handler import hats_handler
 
 
 def get_timers(send_message_func):
@@ -11,5 +12,7 @@ def get_timers(send_message_func):
 
 def get_command_handlers(send_message_func):
 	command_handlers = [
-	dad_jokes_teller.DadJokesTeller, uwu.UwUHandler, dynamic_dictionary.DynamicDictionary, soundbot.Soundbot]
+	dad_jokes_teller.DadJokesTeller, uwu.UwUHandler, dynamic_dictionary.DynamicDictionary,
+	soundbot.Soundbot, hats_handler.HatsHandler]
+
 	return [command_handler(send_message_func, CooldownManager()) for command_handler in command_handlers]

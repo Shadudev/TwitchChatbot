2b358ee2fd563d28bb70f9cff7d7e6c11f2af31e
Ofir Cohen <ofirgvc@gmail.com>
2020-03-25 09:06:39 +0200

    Added starting message

--- chatbot.py
+++ chatbot.py
@@ -10,7 +10,7 @@ from utils.twitch_socket import TwitchSocket
 
 class Chatbot(object):
 
-	_interval = timedelta(seconds=10)
+	_timers_check_interval = timedelta(seconds=10)
 	_chatbot = None
 
 	def __init__(self):
@@ -21,11 +21,17 @@ class Chatbot(object):
 		self._is_running = True
 		self._timers = scripts.get_timers(Chatbot.send_message)
 		self._command_handlers = scripts.get_command_handlers(Chatbot.send_message)
+		self._timer_thread = threading.Thread(target=self.handle_timers)
 
 	def serve_forever(self):
-		self._timer_thread = threading.Thread(target=self.handle_timers)
+		print("Chatbot starting...")
 		self._timer_thread.start()
+		self._handle_chat_messages()
 
+		self._twitch_socket.close()
+		self._timer_thread.join()
+
+	def _handle_chat_messages(self):
 		while self._is_running:
 			chat_message = self._twitch_socket.recv_message()
 			print(chat_message.display_name + ': ' + chat_message.message)
@@ -40,13 +46,10 @@ class Chatbot(object):
 			if chat_message.message == 'Chatbot, stop.' and chat_message.is_mod:
 				self._is_running = False
 
-		self._twitch_socket.close()
-		self._timer_thread.join()
-
 	def handle_timers(self):
 		timers_ticks = {timer: timedelta(0) for timer in self._timers}
 
-		sleep(self._interval.seconds)
+		sleep(self._timers_check_interval.seconds)
 		while self._is_running:
 			for timer in self._timers:
 				try:
@@ -54,11 +57,11 @@ class Chatbot(object):
 						self.send_message(timer.get_message())
 						timers_ticks[timer] = timedelta(0)
 					else:
-						timers_ticks[timer] += self._interval
+						timers_ticks[timer] += self._timers_check_interval
 				except Exception as e:
 					print(traceback.format_exc())
 	
-			sleep(self._interval.seconds)
+			sleep(self._timers_check_interval.seconds)
 
 
 	def inst_send_message(self, message):

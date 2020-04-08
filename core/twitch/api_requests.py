7f59b64a0bccc57a5292b1dbeb62ede7e52851cb
Ofir Cohen <ofirgvc@gmail.com>
2020-04-09 02:20:25 +0300

    Made a function for getting channel id in api_requests

--- core/twitch/api_requests.py
+++ core/twitch/api_requests.py
@@ -3,15 +3,13 @@ import requests
 from core.framework import configuration
 
 
-def get_stream_category():
+def get_channel_id(channel_name=configuration.get_channel_name()):
 	twitch_headers = {'Client-ID': 'q6batx0epp608isickayubi39itsckt', 'Accept': 'application/vnd.twitchtv.v5+json'}
-	response = requests.get('https://api.twitch.tv/kraken/users?login=' + configuration.get_channel_name(), headers=twitch_headers)
-	channel_id = response.json()['users'][0]['_id']
+	response = requests.get('https://api.twitch.tv/kraken/users?login=' + channel_name, headers=twitch_headers)
+	return response.json()['users'][0]['_id']
+
+
+def get_stream_category(channel_name=configuration.get_channel_name()):
+	channel_id = get_channel_id(channel_name)
 	response = requests.get('https://api.twitch.tv/kraken/channels/' + channel_id, headers=twitch_headers)
 	return response.json()['game']
-
-
-def get_channel_id(channel_name):
-	twitch_headers = {'Client-ID': 'q6batx0epp608isickayubi39itsckt', 'Accept': 'application/vnd.twitchtv.v5+json'}
-	response = requests.get('https://api.twitch.tv/kraken/users?login=' + configuration.get_channel_name(), headers=twitch_headers)
-	return response.json()['users'][0]['_id']

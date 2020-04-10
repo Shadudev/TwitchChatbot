8b5ae98923c2ef0195e807a519b006bebee60d85
Ofir Cohen <ofirgvc@gmail.com>
2020-04-10 22:08:01 +0300

    Exported twitch_headers in api_requests to a getter function

--- core/twitch/api_requests.py
+++ core/twitch/api_requests.py
@@ -3,13 +3,16 @@ import requests
 from core.framework import configuration
 
 
+def get_twitch_headers():
+	return {'Client-ID': 'q6batx0epp608isickayubi39itsckt', 'Accept': 'application/vnd.twitchtv.v5+json'}
+
+
 def get_channel_id(channel_name=configuration.get_channel_name()):
-	twitch_headers = {'Client-ID': 'q6batx0epp608isickayubi39itsckt', 'Accept': 'application/vnd.twitchtv.v5+json'}
-	response = requests.get('https://api.twitch.tv/kraken/users?login=' + channel_name, headers=twitch_headers)
+	response = requests.get('https://api.twitch.tv/kraken/users?login=' + channel_name, headers=get_twitch_headers())
 	return response.json()['users'][0]['_id']
 
 
 def get_stream_category(channel_name=configuration.get_channel_name()):
 	channel_id = get_channel_id(channel_name)
-	response = requests.get('https://api.twitch.tv/kraken/channels/' + channel_id, headers=twitch_headers)
+	response = requests.get('https://api.twitch.tv/kraken/channels/' + channel_id, headers=get_twitch_headers())
 	return response.json()['game']

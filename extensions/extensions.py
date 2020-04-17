86ea31d55ea2191b9671c5f30dd2793a6882d1d2
Ofir Cohen <ofirgvc@gmail.com>
2020-04-17 20:55:44 +0300

    Added smallscreen command

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -5,6 +5,7 @@ from extensions.dad_jokes_teller import dad_jokes_teller
 from extensions.uwu import uwu, dynamic_dictionary
 from extensions.soundbot import soundbot
 from extensions.hats_handler import hats_handler
+from extensions.small_screen import small_screen
 
 
 def get_timers(send_message_func):
@@ -13,6 +14,6 @@ def get_timers(send_message_func):
 def get_command_handlers(send_message_func):
 	command_handlers = [
 	dad_jokes_teller.DadJokesTeller, uwu.UwUHandler, dynamic_dictionary.DynamicDictionary,
-	soundbot.Soundbot, hats_handler.HatsHandler]
+	soundbot.Soundbot, hats_handler.HatsHandler, small_screen.SmallScreenHandler]
 
 	return [command_handler(send_message_func, CooldownManager()) for command_handler in command_handlers]
